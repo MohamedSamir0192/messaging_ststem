6a486f02baa240c915ec14c3efaf1286664d7ca9
mohamedsamir0192 <someone@example.com>
2016-08-25 13:17:30 +0200

    new commit

---
+++ deploy.php
@@ -0,0 +1,13 @@
+<?php /*
+$working_path = "C:\xampp\htdocs\messaging_system";
+$result = array();
+exec("git status;", $result);
+print_r($result);
+*/
+$commit_msg = $_GET['msg'];
+
+echo exec('git add --all');
+echo exec('git commit -m '.$commit_msg);
+echo exec('git pull origin1 master');
+
+?>
